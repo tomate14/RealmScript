66966858164de0b87c5ced6cfcee56658e24d179
Maximiliano Roselli <maxiroselli@gmail.com>
2023-02-25 17:48:45 -0300

    Open extension on realm page

--- background.js
+++ background.js
@@ -5,8 +5,19 @@
 // https://developers.google.com/open-source/licenses/bsd
 
 // Show the demo page once the extension is installed
-chrome.runtime.onInstalled.addListener((_reason) => {
-    chrome.tabs.create({
-      url: 'demo/index.html'
-    });
-});
+/*chrome.runtime.onInstalled.addListener((_reason) => {
+  chrome.tabs.create({
+    url: 'demo/index.html'
+  });
+});*/
+
+chrome.tabs.onUpdated.addListener(
+  function ( tabId, changeInfo, tab )
+  { 
+    if ( changeInfo.url === 'https://play.realmnft.io/' )
+    {
+      chrome.tabs.create({
+        url: 'demo/index.html'
+      });
+    }
+  });
